2c48b5003d4157d3f3fe7111aa3328fa4c5899be
Filip Karlo Došilović <filipk.dosilovic@gmail.com>
2021-01-15 15:53:30 +0100

    Add DiscreteMLPAgent to agent module.

--- src/agent.py
+++ src/agent.py
@@ -5,6 +5,19 @@ from sklearn.preprocessing import PolynomialFeatures
 from scipy.special import binom
 
 
+def __create_multilayer_perceptron(layers_dim):
+    layers = []
+    for i, layer_d in layers_dim[1:]:
+        layers.append(torch.nn.Linear(layers_dim[i], layer_d))
+        layers.append(torch.nn.ReLU(inplace=True))
+
+    layers = layers[:-1]  # Remove the ReLU after the output layer.
+    layers.append(torch.nn.Softmax(dim=1))
+    layers.append(torch.Flatten(start_dim=0))
+
+    return torch.nn.Sequential(*layers)
+
+
 class DiscreteLinearAgent:
     def __init__(self, n_features, n_actions, degree=1):
         n_features = int(binom(n_features + degree, degree))
@@ -32,3 +45,24 @@ class DiscreteLinearAgent:
             state = torch.as_tensor(state, dtype=torch.float32)
 
         return torch.argmax(self.policy.forward(state)), None
+
+
+class DiscreteMLPAgent:
+    """Implements an agent with neural network policy."""
+
+    def __init__(self, layers):
+        self.policy = __create_multilayer_perceptron(layers)
+
+    def sample_action(self, state):
+        """Samples an action."""
+        if not isinstance(state, torch.Tensor):
+            state = torch.as_tensor(state, dtype=torch.float32)
+
+        probs = self.policy(state)
+        return (torch.multinomial(probs, 1), probs)
+
+    def choose_action(self, state):
+        if not isinstance(state, torch.Tensor):
+            state = torch.as_tensor(state, dytpe=torch.float32)
+
+        return torch.argmax(self.policy(state)), None
